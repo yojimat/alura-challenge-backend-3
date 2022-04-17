a975163d50278b46f8e512856793453ef9af6747
yojimat <31702569+yojimat@users.noreply.github.com>
2022-04-17 17:11:11 -0300

    Replacing template

---
+++ Alura Challenge Backend 3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Alura_Challenge_Backend_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
